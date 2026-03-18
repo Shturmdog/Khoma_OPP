82888df67ff10b018801353799a4e8f5ab6342db
Gleb <gleb.homa6@gmail.com>
2026-03-18 16:01:36 +0800

    Added main.cpp with the working algorithm

---
+++ konteynery/main.cpp
@@ -0,0 +1,23 @@
+﻿#include "Array.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+	int k = 3;
+	Array arr(9);
+	for (int i = 0; i < 9; i++) {
+		arr.insert(i + 1);
+	}
+
+	int size = arr.getSize();
+	
+	int i = 0;
+	while (size > 1) {
+		i = (i + k - 1) % size;
+		arr.remove(i);
+		size--;
+		cout << arr;
+	}
+
+	return 0;
+ }
