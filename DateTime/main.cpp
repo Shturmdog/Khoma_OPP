a0fb92aa67e5cfaedec2362ef777394651e1010d
Gleb <gleb.homa6@gmail.com>
2026-04-07 20:02:52 +0800

    Added setlocal and  func textformat in main

--- DateTime/main.cpp
+++ DateTime/main.cpp
@@ -3,9 +3,11 @@
 using namespace std;
 
 int main() {
+	setlocale(LC_ALL, "RU");
 	DateTime data;
 	data.input();
 	data.basicFormat();
+	data.textformat();
 	data.shortformat();
 	cout << data.DateEaster();
 }
