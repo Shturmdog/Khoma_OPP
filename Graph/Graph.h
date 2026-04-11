f7869d79fae3f2cf8162a1e13d93ca48cb294c65
Gleb <gleb.homa6@gmail.com>
2026-04-11 16:57:27 +0800

    Syntax errors have been fixed

--- Graph/Graph.h
+++ Graph/Graph.h
@@ -3,12 +3,31 @@
 #include <set>
 using namespace std;
 
+class Node {
+	string name;
+	set<Node*> neighbours;
+	void addNeighbour(Node* neighbour);
+	void removeNeighbour(Node* neighbour);
+public:
+	typedef set<Node*>::const_iterator node_iterator;
 
-typedef set<Node*>::const_iterator node_iterator;
+	Node(const string& aname) : name(aname) {};
+	const string& getname() const { return name; }
+	node_iterator nb_begin() const {
+		return neighbours.begin();
+	}
+	node_iterator nb_end() const {
+		return neighbours.end();
+	}
+	friend class Graph;
+};
+
 class Graph {
 	set<Node*> nodes;
 public:
-	void addNode(Node*, node);
+	typedef set<Node*>::const_iterator node_iterator;
+
+	void addNode(Node* node);
 	void removeNode(Node* node);
 	void addEdge(Node* begin, Node* end);
 	void removeEdge(Node* begin, Node* end);
@@ -19,21 +38,4 @@ public:
 	node_iterator end() const {
 		return nodes.end();
 	}
-};
-
-class Node {
-	string name;
-	set<Node*> neighbours;
-	void addNeighbour(Node* neighbour);
-	void removeNeighbour(Node* neighbour);
-public:
-	Node(const string& aname) : name(aname) {};
-	const string& getname() const { return name; }
-	node_iterator nb_begin() const {
-		return neighbours.begin();
-	}
-	node_iterator nb_end() const {
-		return neighbours.end();
-	}
-	friend class Graph;
 };
