719d2c5c55c37bb8d9d39fea966872885336d990
Gleb <gleb.homa6@gmail.com>
2026-04-20 19:21:12 +0800

    Added func for Elipse

--- Shape/Shape.h
+++ Shape/Shape.h
@@ -33,3 +33,32 @@ public:
 		cout << "Circle" << endl;
 	}
 };
+
+class Elipse : public Figure {
+	double center_x;
+	double center_y;
+	double radius_a;
+	double radius_b;
+public:
+	Elipse(double cx, double cy, double ra, double rb) {
+		center_x = cx;
+		center_y = cy;
+		radius_a = ra;
+		radius_b = rb;
+	}
+
+	double calc_area() {
+		return PI * radius_a * radius_b;
+	}
+
+	double calc_perimetr() {
+		double a = radius_a;
+		double b = radius_b;
+		double h = ((a - b) * (a - b)) / ((a + b) * (a + b));
+		return PI * (a + b) * (1 + (3 * h) / (10 + sqrt(4 - 3 * h)));
+	}
+
+	void name() {
+		cout << "Elipse" << endl;
+	}
+};
