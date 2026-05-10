ec1eb28b5dc75ba103a6e080a3cd6524b8080168
Gleb <gleb.homa6@gmail.com>
2026-05-10 22:33:26 +0800

    Updated file Field.h

--- Field/Field.h
+++ Field/Field.h
@@ -2,6 +2,13 @@
 #include <iostream>
 using namespace std;
 
+const int FIELD_WIDTH = 7;
+const int FIELD_HEIGHT = 6;
+const int WIN_LENGTH = 4;
+const int DIR_NUMBER = 4;
+const int di[] = { 1, 0, 1, 1 };
+const int dj[] = { 0, 1, -1, 1 };
+
 enum Cell {
 	EMPTY = 0,
 	RED = 1,
@@ -19,4 +26,9 @@ public:
 	bool isRedTurnNow() const;
 	void print() const;
 	void printResult() const;
+
+	void checkWinner();
+	Cell cells[FIELD_WIDTH][FIELD_HEIGHT];
+	bool isRedTurn;
+	Cell winner;
 };
