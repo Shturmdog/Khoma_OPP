8cd05212815d73aac98573df713c78cd61cd0065
Gleb <gleb.homa6@gmail.com>
2026-05-30 18:10:10 +0800

    vector<vector<int, int>> emptyCells -> vector<pair<int, int>> emptyCells

--- TwentyFortyEight/TwentyFortyEight.cpp
+++ TwentyFortyEight/TwentyFortyEight.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 void Game::AddTile() {
-	vector<vector<int, int>> emptyCells;
+	vector<pair<int, int>> emptyCells;
 	for (int i = 0; i <= 4; i++) {
 		for (int j = 0; j <= 4; j++) {
 			emptyCells.push_back({ i, j });
