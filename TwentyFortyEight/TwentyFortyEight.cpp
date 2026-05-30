f638638dd3a0253616d39fbbe8ac1870c8883930
Gleb <gleb.homa6@gmail.com>
2026-05-30 18:03:01 +0800

    Added cycle in func AddTile

--- TwentyFortyEight/TwentyFortyEight.cpp
+++ TwentyFortyEight/TwentyFortyEight.cpp
@@ -6,4 +6,10 @@ using namespace std;
 
 void Game::AddTile() {
 	vector<vector<int, int>> emptyCells;
+	for (int i = 0; i <= 4; i++) {
+		for (int j = 0; j <= 4; j++) {
+			emptyCells.push_back({ i, j });
+		}
+	}
+	int value = (rand() % 10) ? 4 : 2;
 }
