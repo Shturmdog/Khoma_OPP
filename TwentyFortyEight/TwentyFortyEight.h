ee691f94b7b2cbc655348f46febdb67599f15a83
Gleb <gleb.homa6@gmail.com>
2026-05-31 01:13:47 +0800

    Added func for moove

--- TwentyFortyEight/TwentyFortyEight.h
+++ TwentyFortyEight/TwentyFortyEight.h
@@ -2,11 +2,22 @@
 #include <iostream>
 
 class Game {
-	int board[4][4];
-	int score;
-	bool moved;
+	int board[4][4] = {};
+	int score = 0;
+	bool moved = false;
 
 	void AddTile();
 	void ClearBourd();
 	void LeftShift();
+	void MergeLeft();
+	void ReverseRows();
+	void Transpose();
+	void MoveLeft();
+	void MoveRight();
+	void MoveUp();
+	void MoveDown();
+
+public:
+	void Game2048();
+	void play();
 };
